22cef194b61709f12a23f968a7618a646a05077c
KEISUKENEMOTO <ndr5024@gmail.com>
2020-05-25 11:31:04 +0900

    ひとこと掲示板 postsテーブルの値(member_idやmessageなど)に具体的な値を入れないとinsertできない状態。

---
+++ memomemomemo/input_do.php
@@ -0,0 +1,49 @@
+<?php require('dbconnect.php'); ?>
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>よくわかるPHPの教科書</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+    </header>
+
+    <main>
+        <h2>Practice</h2>
+        <pre>
+<?php
+/* ここに、PHPのプログラムを記述します */
+try {
+    $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf8', 'root', 'root');
+} catch (PDOException $e) {
+    echo 'DB接続エラー:' . $e->getMessage();
+}
+
+// $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
+
+$statement = $db->prepare('INSERT INTO memos SET memo=?,created_at=NOW()');
+$statement->execute(array($_POST['memo']));
+echo 'メモが登録されました';
+//上記のプログラムはbindParamでも既述することができる。PDOステートメントオブジェクト->bindParam(パラメータの順番,値);
+// $statement = $db->prepare('INSERT INTO memos SET memo=?,created_at=NOW()'); //この行は一緒。
+// $statement->bindParam(1, $_POST['memo']); //array使わない
+// $statement->execute();
+
+
+
+?>
+</pre>
+    </main>
+</body>
+
+</html>
---
+++ post/join/選択項目から作成したフォルダ/thanksのコピー.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ひとこと掲示板</title>
+
+    <link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+    <p>ユーザー登録が完了しました。</p>
+    <p><a href="../">ログインする</a></p>
+</body>
+
+</html>
