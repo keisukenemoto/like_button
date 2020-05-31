618ee982e06065ea06d9c73eb85cccd9d15b5ad8
KEISUKENEMOTO <ndr5024@gmail.com>
2020-06-01 00:13:15 +0900

    タイムラインの時刻表示修正

--- post/index.php
+++ post/index.php
@@ -23,6 +23,9 @@
             if (empty($_POST['reply_post_id'])) {
                 $_POST['reply_post_id'] = 0;
             }
+            // if ($_POST['reply_post_id'] = '') {
+            //     $_POST['reply_post_id'] = 0;
+            // }//なぜこれではダメ？
 
             // var_dump(array(
             //     $member['id'],
@@ -126,7 +129,7 @@
                  <div class="msg">
                      <img src="member_picture/<?php echo h($post['picture']); ?>" width="48" height="48" alt="<?php echo h($post['name']); ?>" />
                      <p><?php echo makeLink(h($post['message'])); ?><span class="name">(<?php echo h($post['name']); ?>)</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]</p>
-                     <p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($member['created']); ?></a>
+                     <p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a>
                          <?php
                             if ($post['reply_post_id'] > 0) :
                             ?>
